208c20bb1e4f4353f15c3e152fdd2c3c6faf6edb
hanaraya <n.harish22@gmail.com>
2015-02-02 23:37:47 -0800

    Add meteor changes

--- todos.js
+++ todos.js
@@ -1,22 +1,55 @@
+var Tasks = new Meteor.Collection("tasks");
+
+
+Meteor.methods({
+  createTask: function(text) {
+    Tasks.insert({
+      text: text,
+      createdDate: new Date()
+    })
+
+  },
+
+  deleteTask: function(taskId){
+    Tasks.remove(taskId);
+  }
+
+});
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function() {
-      return Session.get('counter');
+  Meteor.subscribe("tasks");
+  Template.body.helpers({
+    tasks: function() {
+      return Tasks.find({}, {
+        sort: {
+          createdDate: -1
+        }
+      });
     }
   });
 
-  Template.hello.events({
-    'click button': function() {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  Template.body.events({
+    "submit .new-task": function(event) {
+      var text = event.target.text.value;
+      Meteor.call("createTask", text);
+      event.target.text.value = "";
+      return false;
     }
   });
+
+  Template.task.events({
+    "click .delete": function(){
+      Meteor.call("deleteTask",this._id);
+    }
+  });
+
+
 }
 
 if (Meteor.isServer) {
+  Meteor.publish("tasks", function() {
+    return Tasks.find();
+  });
   Meteor.startup(function() {
     // code to run on server at startup
   });
