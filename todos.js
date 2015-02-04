0c3d67f4f69a6702620091a4686c55764ccc7117
hanaraya <n.harish22@gmail.com>
2015-02-03 23:29:09 -0800

    new changes add task count and hide completed tasks

--- todos.js
+++ todos.js
@@ -5,13 +5,18 @@ Meteor.methods({
   createTask: function(text) {
     Tasks.insert({
       text: text,
-      createdDate: new Date()
+      createdDate: new Date(),
+      checked: false
     })
 
   },
 
   deleteTask: function(taskId){
     Tasks.remove(taskId);
+  },
+
+  toggleChecked: function(taskId, checked){
+    Tasks.update(taskId,{$set: {checked: checked}})
   }
 
 });
@@ -20,12 +25,21 @@ if (Meteor.isClient) {
   Meteor.subscribe("tasks");
   Template.body.helpers({
     tasks: function() {
+      if(Session.get("hideCompleted")){
+        return Tasks.find({checked: {$ne: true}},{sort: {
+          createdDate: -1
+        }})
+      }
+      else{
       return Tasks.find({}, {
         sort: {
           createdDate: -1
         }
-      });
-    }
+      })}},
+      incompleteCount: function(){
+        return Tasks.find({checked: {$ne: true}}).count();
+      }
+
   });
 
   Template.body.events({
@@ -34,12 +48,18 @@ if (Meteor.isClient) {
       Meteor.call("createTask", text);
       event.target.text.value = "";
       return false;
+    },
+    "change .hide-completed input": function(event){
+      Session.set("hideCompleted",event.target.checked)
     }
   });
 
   Template.task.events({
     "click .delete": function(){
       Meteor.call("deleteTask",this._id);
+    },
+    "click .toggle-checked": function(){
+      Meteor.call("toggleChecked",this._id, ! this.checked);
     }
   });
 
